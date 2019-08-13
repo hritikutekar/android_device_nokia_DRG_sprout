e6d8905340439576c06741caf5d2efd70c3d7b83
Raghu Varma <raghuvarma331@gmail.com>
2019-08-13 17:56:39 +0530

    DRG_sprout: add official support

--- aosp_DRG_sprout.mk
+++ aosp_DRG_sprout.mk
@@ -23,6 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 $(call inherit-product, device/nokia/DRG_sprout/device.mk)
 
 # Inherit some common OnUI-Pendro stuff.
+export CUSTOM_BUILD_TYPE=OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
