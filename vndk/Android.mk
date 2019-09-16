1fc58080425e8ac8f9df8b08781a87409971bc91
hritik <hritikdrocks@gmail.com>
2019-09-16 18:53:05 +0000

    DRG_sprout: vndk: merge changes on marlin for Android 10
    Reference:
    
    https://android.googlesource.com/device/google/marlin/+/refs/tags/android-10.0.0_r2/vndk/Android.mk

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,14 +1,25 @@
+# TODO(b/69526027): DEPRECATE USE OF THIS.
+# USE BOARD_VNDK_VERSION:=current instead.
+
 LOCAL_PATH := $(call my-dir)
 
+# b/69526027: This VNDK-SP install routine must be removed. Instead, we must
+# build vendor variants of the VNDK-SP modules.
+
 ifndef BOARD_VNDK_VERSION
+# The libs with "vndk: {enabled: true, support_system_process: true}" will be
+# added VNDK_SP_LIBRARIES automatically. And the core variants of the VNDK-SP
+# libs will be copied to vndk-sp directory.
+# However, some of those libs need FWK-ONLY libs, which must be listed here
+# manually.
 
 VNDK_SP_LIBRARIES := \
-    libdexfile \
-    libartbase \
-    libziparchive
+   libdexfile_support \
 
 install_in_hw_dir := \
    android.hidl.memory@1.0-impl
+
+vndk_sp_dir := vndk-sp-$(PLATFORM_VNDK_VERSION)
 
 define define-vndk-sp-lib
 include $$(CLEAR_VARS)
@@ -21,7 +32,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_VENDOR_MODULE := true
-LOCAL_MODULE_RELATIVE_PATH := vndk-sp
+LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)$(if $(filter $1,$(install_in_hw_dir)),/hw)
+LOCAL_CHECK_ELF_FILES := false
 include $$(BUILD_PREBUILT)
 
 ifneq ($$(TARGET_2ND_ARCH),)
@@ -36,7 +48,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_VENDOR_MODULE := true
-LOCAL_MODULE_RELATIVE_PATH := vndk-sp
+LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)$(if $(filter $1,$(install_in_hw_dir)),/hw)
+LOCAL_CHECK_ELF_FILES := false
 include $$(BUILD_PREBUILT)
 endif # TARGET_TRANSLATE_2ND_ARCH is not true
 endif # TARGET_2ND_ARCH is not empty
